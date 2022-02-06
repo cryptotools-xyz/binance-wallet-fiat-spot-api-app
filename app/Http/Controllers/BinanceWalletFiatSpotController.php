a09b298afa9028ee21fb36edc7fadf3a8746fd13
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-02-06 19:17:19 +0100

    Setup the api call to get account info

---
+++ app/Http/Controllers/BinanceWalletFiatSpotController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Http;
+
+class BinanceWalletFiatSpotController extends BinanceController
+{
+    public function index()
+    {
+        $publicKey = $this->binance_api_key;
+        $secretKey = $this->binance_secret_key;
+
+        $timestamp = round(microtime(true) * 1000);
+
+        $parameters['timestamp'] = $timestamp;
+        $query = $this->buildQuery($parameters);
+        $signature = $this->signature($query, $secretKey);
+
+        $response = Http::withHeaders([
+            'X-MBX-APIKEY' => $publicKey
+        ])->get("https://api.binance.com/api/v3/account?timestamp=$timestamp&signature=$signature");
+
+        return $response->json();
+    }
+}
